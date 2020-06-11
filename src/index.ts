fe3a0ad14e63260ed3a4dc9c1ac44a9d7bd993c8
Jason Favrod <jason.favrod@monsanto.com>
2020-06-11 09:53:29 -1000

    Started Implementation of post Function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import http from 'http';
+import http, { IncomingMessage } from 'http';
 import https from 'https';
 import { IResponse } from './interfaces';
+import { URL } from 'url';
 
 const endMethod = (incomming: http.IncomingMessage, data: string, response: IResponse, resolve: (response: IResponse) => any): void => {
     response.headers = incomming.headers;
@@ -54,6 +55,52 @@ export function get(url: string, options?: http.RequestOptions): Promise<IRespon
             });
         });
     }
+
+    return promise;
+}
+
+export function post(url: string, options?: http.RequestOptions): Promise<IResponse> {
+    const response = {} as IResponse;
+
+    let data: string;
+    let module: any; // either http or https.
+    let parsedUrl: URL;
+    let promise: Promise<IResponse>;
+
+    options = options ? options : {};
+
+    parsedUrl = new URL(url);
+    console.log('url', url);
+    console.log('parsedUrl', parsedUrl);
+    options = Object.assign(
+        {
+            host: parsedUrl.hostname,
+            method: 'POST',
+            path: parsedUrl.pathname,
+            port: parsedUrl.protocol === 'https:' ? 443 : 80,
+        },
+        options,
+    );
+    console.log('options', options);
+
+    if (options!.port === 443) {
+        module = https;
+    }
+    else {
+        module = http;
+    }
+
+    promise = new Promise<IResponse>((resolve) => {
+        module.request(options, (res: IncomingMessage) => {
+            res.setEncoding('utf-8');
+
+            res.on('data', (chunk: any) => {
+                data += chunk;
+            });
+
+            res.on('end', () => endMethod(res, data, response, resolve));
+        });
+    });
 
     return promise;
 }
