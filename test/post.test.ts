34d71dc7732352d8ef719b217c64417172cfd0fb
Jason Favrod <mail@jasonfavrod.com>
2020-06-25 21:13:16 -1000

    Added post Test
    For POSTing URL encoded form data.

--- test/post.test.ts
+++ test/post.test.ts
@@ -1,14 +1,38 @@
 import assert from 'assert'
+import querystring from 'querystring';
 import { post } from '../src';
 import { isIResponse } from '../src/typegaurds';
 
 describe('post JSON https', () => {
-    it('Should return an IResponse with string data', async () => {
+    it('Should return an IResponse with data', async () => {
         const res = await post('https://postman-echo.com/post', {
             data: JSON.stringify({
                 hello: "world",
-            })
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            },
         });
+
+        assert.strictEqual(isIResponse(res), true);
+        assert.strictEqual(typeof(res.data), 'object');
+    });
+});
+
+describe('post URL encoded form data', () => {
+    const form = querystring.encode({
+        firstname: 'jason',
+        lastname: 'favrod',
+    });
+
+    it('Should return an IResponse with string data', async () => {
+        const res = await post('https://postman-echo.com/post', {
+            data: form,
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+        });
+
         assert.strictEqual(isIResponse(res), true);
         assert.strictEqual(typeof(res.data), 'object');
     });
